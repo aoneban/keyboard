ac56938e34b96a1809d3da68c5126f84986192e9
aoneban <torgregion1@gmail.com>
2023-04-26 22:41:48 +0200

    feat: created function for CapsLock

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const area = document.getElementById('input');
 
-init()
+init();
 function init() {
   document.onkeydown = function (event) {
     console.log(event.code);
@@ -14,14 +14,76 @@ function init() {
   };
 }
 
-const keys = document.querySelectorAll('.key');
-
-keys.forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    this.classList.add('active');
-    area.value += this.firstElementChild.innerText;
-    setTimeout(() => {
-      this.classList.remove('active');
-    }, 200);
+initMouseCapsLockEn();
+function initMouseCapsLockEn() {
+  const capsLock = document.querySelector('.CapsLock');
+  capsLock.addEventListener('click', function () {
+    const caseDown = document.querySelectorAll('.caseDown');
+    const caps = document.querySelectorAll('.caps');
+    if (
+      capsLock.classList.contains('CapsLock') &&
+      !capsLock.classList.contains('active')
+    ) {
+      capsLock.classList.add('active');
+      caseDown.forEach(function (el) {
+        el.style.display = 'none';
+      });
+      caps.forEach(function (el) {
+        el.style.display = 'block';
+      });
+    } else {
+      capsLock.classList.remove('active');
+      caps.forEach(function (el) {
+        el.style.display = 'none';
+      });
+      caseDown.forEach(function (el) {
+        el.style.display = 'block';
+      });
+    }
   });
-});
+}
+
+initMouse();
+function initMouse() {
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(function (el) {
+    if (!el.classList.contains('CapsLock')) {
+      el.addEventListener('click', function () {
+        this.classList.add('active');
+        area.value += this.firstElementChild.innerText;
+        setTimeout(() => {
+          this.classList.remove('active');
+        }, 200);
+      });
+    }
+  });
+}
+
+initCapsLockEn();
+function initCapsLockEn() {
+  document.onkeydown = function (event) {
+    const capsLock = document.querySelector('.CapsLock');
+    const caseDown = document.querySelectorAll('.caseDown');
+    const caps = document.querySelectorAll('.caps');
+    if (event.code === 'CapsLock' && !capsLock.classList.contains('active')) {
+      capsLock.classList.add('active');
+      caseDown.forEach(function (el) {
+        el.style.display = 'none';
+      });
+      caps.forEach(function (el) {
+        el.style.display = 'block';
+      });
+    } else if (
+      event.code === 'CapsLock' &&
+      capsLock.classList.contains('active')
+    ) {
+      capsLock.classList.remove('active');
+      caps.forEach(function (el) {
+        el.style.display = 'none';
+      });
+      caseDown.forEach(function (el) {
+        el.style.display = 'block';
+      });
+    }
+  };
+}
