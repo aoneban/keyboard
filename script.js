d14067ff82540d83ba9a63ef31bc45b43421da58
aoneban <torgregion1@gmail.com>
2023-05-02 20:07:18 +0200

    feat: add functionality to save to localstorage

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { data } from "./data.js";
 
-const genereteKeyboard = () => {
+let language = localStorage.getItem('lang')
+
+const genereteKeyboard = (lang) => {
 const container = document.createElement('div');
 container.classList.add('container');
 
@@ -18,38 +20,100 @@ textArea.setAttribute('id', 'input');
 textArea.setAttribute('cols', '30');
 textArea.setAttribute('rows', '10');
 
-const row = document.createElement('div');
-row.className = 'keyboard__row';
-
 const description = document.createElement('p');
 description.className = 'description';
 description.textContent = 'The keyboard was created in the Windows operating system. To switch the language combination: left ctrl + alt'
 
+const row = document.createElement('div');
+row.className = 'keyboard__row';
+
 data.map((el) => {
+  if (lang == 'none') {
     const key = document.createElement('div')
-    key.classList.add('keyboard__key', 'key', `${el.keyCode}`)
-    key.innerHTML = `
-      <span class="eng">
-        <span class="caseDown">${el.caseDownEn}</span>
-        <span class="caseUp">${el.caseUpEn}</span>
-        <span class="caps">${el.capsEn}</span>
-        <span class="shiftcaps">${el.shiftcapsEn}</span>
-      </span>
-      <span class="rus">
-        <span class="caseDown">${el.caseDownEn}</span>
-        <span class="caseUp">${el.caseUpEn}</span>
-        <span class="caps">${el.capsEn}</span>
-        <span class="shiftcaps">${el.shiftcapsEn}</span>
-      </span>
-    `
+    key.classList.add('keyboard__key', 'key', `${el.keyCode}`);
+
+    const eng = document.createElement('span');
+    eng.className = 'eng';
+    eng.style.display = 'block';
+    const caseDownEn = document.createElement('span');
+    caseDownEn.className = 'caseDown';
+    caseDownEn.textContent = el.caseDownEn;
+    const caseUpEn = document.createElement('span');
+    caseUpEn.className = 'caseUp';
+    caseUpEn.textContent = el.caseUpEn;
+    const capsEn = document.createElement('span');
+    capsEn.className = 'caps';
+    capsEn.textContent = el.capsEn;
+    const shiftCapsEn = document.createElement('span');
+    shiftCapsEn.className = 'shiftcaps';
+    shiftCapsEn.textContent = el.shiftcapsEn;
+    eng.append(caseDownEn, caseUpEn, capsEn, shiftCapsEn)
+
+    const rus = document.createElement('span');
+    rus.className = 'rus';
+    rus.style.display = 'none'
+    const caseDownRu = document.createElement('span');
+    caseDownRu.className = 'caseDown';
+    caseDownRu.textContent = el.caseDownRu;
+    const caseUpRu = document.createElement('span');
+    caseUpRu.className = 'caseUp';
+    caseUpRu.textContent = el.caseUpRu;
+    const capsRu = document.createElement('span');
+    capsRu.className = 'caps';
+    capsRu.textContent = el.capsRu;
+    const shiftCapsRu = document.createElement('span');
+    shiftCapsRu.className = 'shiftcaps';
+    shiftCapsRu.textContent = el.shiftcapsRu;
+    rus.append(caseDownRu, caseUpRu, capsRu, shiftCapsRu)
+    key.append(eng, rus)
     row.append(key)
+  } else {
+    const key = document.createElement('div')
+    key.classList.add('keyboard__key', 'key', `${el.keyCode}`);
+
+    const eng = document.createElement('span');
+    eng.className = 'eng';
+    eng.style.display = 'none';
+    const caseDownEn = document.createElement('span');
+    caseDownEn.className = 'caseDown';
+    caseDownEn.textContent = el.caseDownEn;
+    const caseUpEn = document.createElement('span');
+    caseUpEn.className = 'caseUp';
+    caseUpEn.textContent = el.caseUpEn;
+    const capsEn = document.createElement('span');
+    capsEn.className = 'caps';
+    capsEn.textContent = el.capsEn;
+    const shiftCapsEn = document.createElement('span');
+    shiftCapsEn.className = 'shiftcaps';
+    shiftCapsEn.textContent = el.shiftcapsEn;
+    eng.append(caseDownEn, caseUpEn, capsEn, shiftCapsEn)
+
+    const rus = document.createElement('span');
+    rus.className = 'rus';
+    rus.style.display = 'block'
+    const caseDownRu = document.createElement('span');
+    caseDownRu.className = 'caseDown';
+    caseDownRu.textContent = el.caseDownRu;
+    const caseUpRu = document.createElement('span');
+    caseUpRu.className = 'caseUp';
+    caseUpRu.textContent = el.caseUpRu;
+    const capsRu = document.createElement('span');
+    capsRu.className = 'caps';
+    capsRu.textContent = el.capsRu;
+    const shiftCapsRu = document.createElement('span');
+    shiftCapsRu.className = 'shiftcaps';
+    shiftCapsRu.textContent = el.shiftcapsRu;
+    rus.append(caseDownRu, caseUpRu, capsRu, shiftCapsRu)
+    key.append(eng, rus)
+    row.append(key)
+  }
 })
 
 keyboardWrapper.append(textArea, row)
 container.append(titleText, keyboardWrapper, description)
 document.body.append(container)
 }
-genereteKeyboard()
+genereteKeyboard(language)
 
 const area = document.getElementById('input');
 const pressedKeys = {};
@@ -228,7 +292,7 @@ const initKeystroke = () => {
         document.querySelector('.AltLeft').classList.add('active');
         document.querySelectorAll('.eng').forEach((el) => (el.style.display = 'none'));
         document.querySelectorAll('.rus').forEach((el) => (el.style.display = 'block'));
-        localStorage.setItem('test', document.querySelectorAll('.rus'));
+        localStorage.setItem('lang', 'block');
         setTimeout(() => {
           document.querySelector('.ControlLeft').classList.remove('active');
           document.querySelector('.AltLeft').classList.remove('active');
@@ -238,6 +302,7 @@ const initKeystroke = () => {
         document.querySelector('.AltLeft').classList.add('active');
         document.querySelectorAll('.eng').forEach((el) => (el.style.display = 'block'));
         document.querySelectorAll('.rus').forEach((el) => (el.style.display = 'none'));
+        localStorage.setItem('lang', 'none');
         setTimeout(() => {
           document.querySelector('.ControlLeft').classList.remove('active');
           document.querySelector('.AltLeft').classList.remove('active');
